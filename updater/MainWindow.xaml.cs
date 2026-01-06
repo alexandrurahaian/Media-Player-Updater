b49e33a666607696b22d02b944c7fe85f0c4d13c
vortex3225 <bozgor.net.net@gmail.com>
2026-01-06 21:57:47 +0200

    Add project files.

---
+++ updater/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using updater.Scripts;
+
+namespace updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool GetAutoUpdateStatus()
+        {
+            string auto_update_file = System.IO.Path.Combine(AppContext.BaseDirectory, "auto_update.cfg");
+            if (System.IO.File.Exists(auto_update_file))
+            {
+                string contents = System.IO.File.ReadAllText(auto_update_file);
+                return contents.Contains("true");
+            }
+            return false;
+        }
+
+        private static void ClearOldBackups()
+        {
+            const int MIN_AGE = 4; // backups older than MIN_AGE days will be deleted.
+            string backupDir = System.IO.Path.Combine(AppContext.BaseDirectory, "Backups");
+            if (!Directory.Exists(backupDir)) Directory.CreateDirectory(backupDir);
+
+            foreach (string file in System.IO.Directory.GetFiles(System.IO.Path.Combine(AppContext.BaseDirectory, "Backups")))
+            {
+                try
+                {
+                    DateTime creationDateUtc = System.IO.File.GetLastWriteTimeUtc(file);
+                    TimeSpan diff = DateTime.UtcNow - creationDateUtc;
+                    if (diff.TotalDays >= MIN_AGE)
+                        System.IO.File.Delete(file);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Could not clear old backup: {file}\n{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            ClearOldBackups();
+            VersionHandler.InitClient(this);
+            InitializeComponent();
+            this.WindowState = WindowState.Minimized;
+            if (GetAutoUpdateStatus() == true)
+                this.WindowState = WindowState.Minimized;
+            SetVersions();
+            CheckUpdates();
+        }
+
+        public void ShowNetworkError()
+        {
+            error_box.Visibility = Visibility.Visible;
+        }
+
+        private async void SetVersions()
+        {
+            string current_ver = VersionHandler.GetCurrentVersion() ?? "?.?.?";
+            string latest_ver = await VersionHandler.GetLatestVersion();
+
+            current_version_box.Text = $"Current version: {current_ver}";
+            latest_version_box.Text = $"Latest version: {latest_ver}";
+        }
+
+        private async void CheckUpdates()
+        {
+            bool is_already_latest = await VersionHandler.IsCurrentLatest();
+            if (is_already_latest)
+            {
+                Process media_proc = new Process();
+                media_proc.StartInfo.FileName = System.IO.Path.Combine(AppContext.BaseDirectory, "Media Player.exe");
+                media_proc.Start();
+                Environment.Exit(0);
+            }
+            else if (GetAutoUpdateStatus() == true)
+                await VersionHandler.CheckForUpdates();
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                this.Activate();
+                update_btn.IsEnabled = true;
+            }
+        }
+
+        private async void update_btn_Click(object sender, RoutedEventArgs e)
+        {
+           await VersionHandler.CheckForUpdates();
+        }
+
+        private void skip_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Process media_proc = new Process();
+            media_proc.StartInfo.FileName = System.IO.Path.Combine(AppContext.BaseDirectory, "Media Player.exe");
+            media_proc.Start();
+            Environment.Exit(0);
+        }
+
+        private void open_changelog_btn_Click(object sender, RoutedEventArgs e)
+        {
+           const string url = "https://github.com/alexandrurahaian/media_player/releases/latest";
+            try
+            {
+                Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+            }
+        }
+    }
+}
---
+++ updater/Scripts/VersionHandler.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace updater.Scripts
+{
+    public static class VersionHandler
+    {
+        private static string? GITHUB_TOKEN = Environment.GetEnvironmentVariable("MP-UPDATER-API-TOKEN");
+        private static readonly string? CURRENT_APP_VERSION = GetCurrentVersion();
+        private static MainWindow mainWindow;
+
+        private static HttpClient client = new HttpClient
+        {
+            Timeout = TimeSpan.FromSeconds(15),
+        };
+
+        public static void InitClient(MainWindow mw)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(GITHUB_TOKEN))
+                {
+                    throw new Exception("Github API Token is not set!");
+                }
+                client.DefaultRequestHeaders.Add("User-Agent", "Media Player Application");
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("token", GITHUB_TOKEN);
+                mainWindow = mw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static bool MakeBackup()
+        {
+            int randId = new Random().Next(999999);
+            string dataFile = Path.Combine(AppContext.BaseDirectory, "media.db");
+            string backupFolder = Path.Combine(AppContext.BaseDirectory, "Backups");
+            string dataBakFile = Path.Combine(backupFolder, $"bak_media-{randId}.db");
+            
+            try
+            {
+                if (!Directory.Exists(backupFolder)) Directory.CreateDirectory(backupFolder);
+                File.Copy(dataFile, dataBakFile, false);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not create backup: {ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+                return false;
+            }
+        }
+
+        private static async void DownloadUpdate()
+        {
+            string download_url = "https://github.com/vortex3225/media_player/releases/latest/download/media_player.zip";
+            string fileToWrite = Path.Combine(AppContext.BaseDirectory, "media_player.zip");
+            string newFolder = Path.Combine(AppContext.BaseDirectory, "media_temp");
+            string tempOldFolder = Path.Combine(AppContext.BaseDirectory, "Old");
+
+            try
+            {
+                bool couldMakeBak = MakeBackup();
+                if (!couldMakeBak)
+                {
+                    MessageBox.Show($"Updating cancelled. Could not make data backup...");
+                    Environment.Exit(0);
+                    return;
+                }
+
+                if (Directory.Exists(tempOldFolder))
+                {
+                    Directory.Delete(tempOldFolder, true);
+                    Directory.CreateDirectory(tempOldFolder);
+                }
+                else Directory.CreateDirectory(tempOldFolder);
+
+                using (var s = await client.GetStreamAsync(new Uri(download_url)))
+                {
+                    using (var fs = new FileStream(fileToWrite, FileMode.CreateNew))
+                    {
+                        await s.CopyToAsync(fs);
+                    }
+                }
+
+                System.IO.Compression.ZipFile.ExtractToDirectory(fileToWrite, newFolder);
+                File.Delete(fileToWrite);
+
+                string[] new_files = Directory.GetFiles(newFolder, "*", SearchOption.TopDirectoryOnly)
+                                              .ToArray();
+                string[] old_files = Directory.GetFiles(AppContext.BaseDirectory);
+
+                string[] persistent_files = { "media.db", "Backups", "auto_update.cfg", "updater.exe", "app_config.json" };
+
+                Debug.WriteLine("Old files: ");
+                foreach (string filePath in old_files)
+                {
+                    Debug.WriteLine(filePath);
+                    string fileName = Path.GetFileName(filePath);
+                    if (persistent_files.Any(p =>
+                        string.Equals(p, fileName, StringComparison.OrdinalIgnoreCase)))
+                        continue;
+                    File.Move(filePath, Path.Combine(tempOldFolder, fileName));
+                }
+
+                Debug.WriteLine("New files: ");
+                foreach (string filePath in new_files)
+                {
+                    Debug.WriteLine(filePath);
+                    string fileName = Path.GetFileName(filePath);
+                    
+                    if (persistent_files.Any(p =>
+                        string.Equals(p, fileName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        File.Delete(filePath);
+                        continue;
+                    }
+
+                    File.Copy(filePath, Path.Combine(AppContext.BaseDirectory, fileName), true);
+                }
+                Process media_player_proc = new Process();
+                media_player_proc.StartInfo.FileName = Path.Combine(AppContext.BaseDirectory, "Media Player.exe");
+                media_player_proc.Start();
+                Directory.Delete(newFolder, true);
+                Directory.Delete(tempOldFolder, true);
+                Environment.Exit(0);
+            }
+            catch (HttpRequestException ex2)
+            {
+                mainWindow.ShowNetworkError();
+                MessageBox.Show($"Failed to download update: {ex2.Message}\n{ex2.StackTrace}\n{ex2.InnerException}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something went wrong while attempting to update to version: {GetLatestVersion()}\n{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+                Debug.WriteLine($"{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+                // deletes download file if something went wrong
+                try
+                {
+                    if (File.Exists(fileToWrite)) File.Delete(fileToWrite);
+                    if (Directory.Exists(newFolder)) Directory.Delete(newFolder, true);
+                }
+                catch (Exception ex2)
+                {
+                    MessageBox.Show($"Cannot delete update files: {ex2.Message}");
+                }
+            }
+        }
+
+        public static async Task<string> GetLatestVersion()
+        {
+            var response = await client.GetStringAsync(
+                              "https://api.github.com/repos/vortex3225/media_player/releases/latest"
+                          );
+            var json = JsonNode.Parse(response);
+            var tag = json?["tag_name"]?.ToString();
+            return string.IsNullOrWhiteSpace(tag)
+                    ? CURRENT_APP_VERSION
+                    : tag.TrimStart('v');
+        }
+
+        public static string? GetCurrentVersion()
+        {
+            string? version = FileVersionInfo.GetVersionInfo(Path.Combine(AppContext.BaseDirectory, "Media Player.exe")).FileVersion;
+            return version;
+        }
+
+        public static async Task<bool> IsCurrentLatest()
+        {
+            Version currentVer = Version.Parse(CURRENT_APP_VERSION);
+            Version fetchedVer = Version.Parse(await GetLatestVersion());
+
+            return currentVer.Equals(fetchedVer);
+        }
+
+        public static async Task CheckForUpdates()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(CURRENT_APP_VERSION))
+                    throw new Exception("Could not fetch current application version!");
+
+                bool is_latest = await IsCurrentLatest();
+                if (is_latest)
+                {
+                    // run media player exe
+                    Process m_proc = new Process();
+                    m_proc.StartInfo.FileName = Path.Combine(AppContext.BaseDirectory, "Media Player.exe");
+                    m_proc.Start();
+                    Environment.Exit(0);
+                }
+                else DownloadUpdate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something went wrong while checking for updates: {ex.Message}\n{ex.StackTrace}");
+            }
+        }
+
+    }
+}
